83dd6e56eeebd5f59e949ffda91c9e1513f7f5c8
rmaguire31 <rmaguire31@gmail.com>
2016-12-20 12:01:03 +0000

    Initial commit.

---
+++ mpg_regression.m
@@ -0,0 +1,13 @@
+function mpg_regression()
+%MPG_REGRESSION MPG regression assignment
+%   Code submission by: Z0966990
+
+% load data from this directory
+mpg = importdata('carmpgdat.txt', '\t', 1);
+
+
+% Do the computing for the assignment....
+
+
+end
+
